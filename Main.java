a12069f8acbef3ab22cc428c41d0027fb81f06b3
William Young <will.young@mail.utoronto.ca>
2023-09-30 15:03:14 -0400

    first draft of http requests

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.*;
+import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
-        
+        String API_URL = "https://lichess.org";
+        String API_TOKEN = "lip_A5fyeDSv7AVQ3nAWngJr";
+
+        try {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI(API_URL))
+                    .GET()
+                    .build();
+
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
